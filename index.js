810c093402b86969c10bb556dde8b53ca8b51206
ochelle <alexizochelle@gmail.com>
2024-01-17 07:35:47 +0300

    Updated commit

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
-let films = [];
-// a function that fetches the movie details from the local server
-function fetchfilms(){
-    fetch("http://localhost:3000/films")
+let moviesInfo = [];
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('http://localhost:3000/films')
         .then(response => response.json())
-        .then(data =>{
-            films = data;
-            displayFilms(films);
-        })
+        .then(data => {
+            moviesInfo = data;
+            displayMovieList(moviesInfo);
+            displayMovieDetails(moviesInfo[0]);
+        });
+});
+
+function displayMovieList(movies) {
+    const flms = document.getElementById('films');
+    flms.innerHTML = '';
+    movies.forEach(movie => {
+        const li = document.createElement('li');
+        li.textContent = movie.title;
+        li.addEventListener('click', () => displayMovieDetails(movie));
+        flms.appendChild(li);
+    });
 }
-// a function that displays the fetched data from the local server conatining films
-function displayFilms(films){
-    const movies = document.getElementById("movies");
-    films.forEach(film => {
-        const listItem = document.createElement("li");
-        listItem.textContent = film.name;
-        movies.appendChild(listItem);
-    })
+
+function displayMovieDetails(movie) {
+    const movieReels = document.getElementById('movieDetails');
+    const availableTickets = movie.capacity - movie.tickets_sold;
+    const buyButton = document.getElementById('buyButton');
+    const soldOutMessage = document.getElementById('soldOutMessage');
+
+    movieReels.innerHTML = `
+        <h2>${movie.title}</h2>
+        <img src="${movie.poster}" alt="${movie.title}">
+        <p>${movie.description}</p>
+        <p>Runtime: ${movie.runtime} minutes</p>
+        <p>Showtime: ${movie.showtime}</p>
+        <p>Available Tickets: ${availableTickets}</p>
+    `;
+
+    movieReels.appendChild(buyButton);
+    movieReels.appendChild(soldOutMessage);
+
+    if (availableTickets > 0) {
+        buyButton.disabled = false;
+    } else {
+        buyButton.disabled = true;
+    }
 }
-// a function that details out the features of each movie from the above listing
-function displayFilmDetails(filmId){
-    fetch (`http://localhost:3000/films/${filmId}`)
-        .then(response => response.json())
-        .then(film => {
-            const container = document.getElementById("movie-details");
-            container.innerHTML =`
-                <label for ="title">Title</label>
-                <input type="text" id="title" title="title" value="${film.title}">
-                <label for ="runtime">Runtime</label>
-                <input type="number" id="runtime" name="runtime" value="${film.runtime}">
-                <label for ="capacity">Capacity</label>
-                <input type="number" id="capacity" title="capacity" value="${film.capacity}">
-                <label for ="showtime">Showtime</label>
-                <input type="time" id="showtime" title="showtime" value="${film.showtime}">
-                <label for ="ticket_sold">Ticket-sold</label>
-                <input type="number" id="ticket_sold" title="ticket_sold" value="${film.ticket_sold}">
-                <label for ="description">Description</label>
-                <input type="text" id="description" title="description" value="${film.description}">
-                <label for ="poster">POster</label>
-                <img src="${film.poster}" alt="${film.title}" id="poster">
-            `;
-        })
-};
-fetchfilms();
+
+function buyTicket(movie) {
+    if ((movie.capacity - movie.tickets_sold) > 0) {
+        movie.tickets_sold++;
+        displayMovieDetails(movie);
+    }
+}
